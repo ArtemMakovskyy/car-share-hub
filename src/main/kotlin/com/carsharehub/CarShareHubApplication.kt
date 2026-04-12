edeb329007d043f636a6d013c705f4f3f00785c3
Artem Makovskyy <photomak79@gmail.com>
2026-04-12 10:59:34 +0300

    init

---
+++ src/main/kotlin/com/carsharehub/CarShareHubApplication.kt
@@ -0,0 +1,11 @@
+package com.carsharehub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarShareHubApplication
+
+fun main(args: Array<String>) {
+    runApplication<CarShareHubApplication>(*args)
+}
---
+++ src/test/kotlin/com/carsharehub/CarShareHubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.carsharehub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarShareHubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
